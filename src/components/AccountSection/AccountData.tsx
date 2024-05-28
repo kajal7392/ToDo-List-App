4aacb0ce567fe6a5d29048cf0fbad995a88f10b7
Kajal Sharma <155152254+kajal7392@users.noreply.github.com>
2024-05-28 14:54:20 +0530

    Initial commit

--- src/components/AccountSection/AccountData.tsx
+++ src/components/AccountSection/AccountData.tsx
@@ -33,10 +33,10 @@ const AccountData: React.FC = () => {
         <TasksDone />
         <DeleteTasks />
         <a
-          href="https://github.com/x1-il"
+          href="https://github.com/kajal7392/ToDo-List-app/tree/master"
           className="mt-4 bg-rose-100 p-2 rounded-md text-rose-600 text-center transition hover:bg-rose-200 dark:bg-slate-700/[.3] dark:text-slate-200"
         >
-          Projected by Ibrahim Laklaa
+          Projected by Kajal Sharma
         </a>
       </section>
     </LayoutMenus>
